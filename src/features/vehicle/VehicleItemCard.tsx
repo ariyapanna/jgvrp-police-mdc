3a0c64cc0713c272f27db8de21237f8d349f1e3a
ariyapanna <ariyapanna@outlook.com>
2026-01-24 23:40:39 +0700

    feat: Add vehicle item card component

---
+++ src/features/vehicle/VehicleItemCard.tsx
@@ -0,0 +1,45 @@
+import { useUser } from "@/context/user.context";
+
+import { Files } from "lucide-react";
+import { toast } from "react-toastify";
+
+import InfoCard from "@/components/cards/InfoCard";
+
+import { getFactionKey } from "@/lib/faction/mapping";
+
+import type { Vehicle } from "@/types/vehicle/vehicle";
+import { FACTION_COLOR } from "@/types/faction/factionColor";
+
+import { copyToClipboard } from "@/utils/clipboard";
+
+interface VehicleItemCardProps 
+{
+    vehicle: Vehicle
+}
+
+const VehicleItemCard = ({ vehicle }: VehicleItemCardProps) => {
+    const { user } = useUser();
+
+    const userFaction = getFactionKey(user?.factionId);
+    const factionColor = userFaction ? FACTION_COLOR[userFaction] : "text-zinc-400";
+    
+    return (
+        <InfoCard
+            title={vehicle.model.toUpperCase()}
+            subtitle={`REG. #${vehicle.id}`}
+            badge={
+                <span className={`flex items-center gap-2 bg-zinc-900 px-3 py-1.5 rounded-lg border border-zinc-800
+                    font-mono text-xs font-bold ${factionColor}`}>
+                    {vehicle.numberPlate}
+                    <Files className="w-3 h-3" />
+                </span>
+            }
+            onBadgeClick={() => {
+                copyToClipboard(vehicle.numberPlate)
+                toast.success("Plate copied");
+            }}
+        />
+    );
+};
+
+export default VehicleItemCard;
